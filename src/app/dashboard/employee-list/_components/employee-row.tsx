05cf55382d237a3da99e444a1efd7d286bfcbc80
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2026-05-11 09:51:18 +0300

    feat: add link to employee detail page

--- src/app/dashboard/employee-list/_components/employee-row.tsx
+++ src/app/dashboard/employee-list/_components/employee-row.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import Link from "next/link";
+
 export default function EmployeeRow({ employee }) {
   return (
     <tr className="border-b border-zinc-100 last:border-0 hover:bg-zinc-50 dark:border-zinc-800 dark:hover:bg-zinc-800/40 transition">
       <td className="px-6 py-4 font-medium text-zinc-950 dark:text-white">
         <div>
-          {employee.firstName} {employee.lastName}
+          <Link
+            className="underline text-cyan-400"
+            href={`/dashboard/employees/${employee.id}`}
+          >
+            {employee.firstName} {employee.lastName}
+          </Link>
         </div>
         <div>{employee.title?.name ?? "Unassigned"}</div>
       </td>
